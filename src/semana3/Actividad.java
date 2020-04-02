dc2e6385c7a39426f1ea6695172f37fae27e5b32
Index <Index@IndexAlpha>
2020-04-02 19:59:12 -0300

    3

--- src/semana3/Actividad.java
+++ src/semana3/Actividad.java
@@ -3,5 +3,26 @@ package semana3;
 public class Actividad {
 	
 	//Atributos
+	
+	private Date dia;
+	private Time horario;
+	private boolean departamental;
+	private boolean nacional;
+	
+	
+	Public Actividad(Date dia, Time horario, boolean departamental, boolean nacional) {
+		
+		this.dia = dia
+		this.horario = horario;
+		this.departamental = departamental;
+		this.nacional = nacional;
+				
+				
+	}
+	
+	
+	//Constructores
+	
+	//Metodos
 
 }
