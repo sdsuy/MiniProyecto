5aa9d156c5273195219dc00e2ef37e40bd9f592a
sdsuy <sdasilva@outlook.es>
2020-04-03 15:40:00 -0300

    elimine el atributo de prueba altura

--- src/semana3/Persona.java
+++ src/semana3/Persona.java
@@ -5,14 +5,14 @@ public class Persona {
 	// atributos
 	private String nombre;
 	private String apellido;
-	private double altura;
+	
 	
 	// constructor
-	public Persona(String nombre, String apellido, double altura) {
+	public Persona(String nombre, String apellido) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
-		this.altura = altura;
+		
 	}
 	
 	// metodos
