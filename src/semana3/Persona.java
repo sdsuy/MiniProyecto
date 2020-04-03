5e911d10d3533986ec7b12fc488373b9432767a6
sdsuy <sdasilva@outlook.es>
2020-04-03 15:45:32 -0300

    Ingresé los atributos faltantes y el metodo de la clase Persona

--- src/semana3/Persona.java
+++ src/semana3/Persona.java
@@ -1,27 +1,36 @@
 package semana3;
 
+import java.util.Date;
+
 public class Persona {
 	
 	// atributos
 	private String nombre;
 	private String apellido;
-	
+	private Date fechaDeNacimiento;
+	private String sexo;
+	private int ci;
+	private String direccion;
+	private int telefono;
 	
 	// constructor
-	public Persona(String nombre, String apellido) {
+	public Persona(String nombre, String apellido, Date fechaDeNacimiento, String sexo, int ci, String direccion,
+			int telefono) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
-		
-	}
-	
-	// metodos
-	public void miMetodo() {
-		
-		// este metodo es solo de muestra y no hace nada
-		
+		this.fechaDeNacimiento = fechaDeNacimiento;
+		this.sexo = sexo;
+		this.ci = ci;
+		this.direccion = direccion;
+		this.telefono = telefono;
 	}
 
-	
+	// metodos
+	public int calcularEdad() {
+		// TODO: Devolver la cantidad de años de this.fechaDeNacimiento - fecha actual
+		//       en formato entero
+		return 0;
+	}
 
 }
