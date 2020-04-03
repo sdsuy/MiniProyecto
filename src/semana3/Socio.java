cdf64b233ea6134607b735d252961c9ee91e09e0
Jose Luis Madrid <Jose Luis Madrid@10.211.55.3>
2020-04-03 16:29:06 -0300

    Quite un atributo altura que no pertenecia a la super clase persona

--- src/semana3/Socio.java
+++ src/semana3/Socio.java
@@ -7,12 +7,11 @@ public class Socio extends Persona {
 	private Date fechaIngreso;
 	private int frecuencia;
 	//Constructor
-public Socio(String nombre, String apellido,Date fechaNacimiento, String sexo, int ci, String direccion,int telefono, double altura, Date fechaIngreso, int frecuencia) {
-	super(nombre, apellido, altura, fechaNacimiento, sexo, ci, direccion, telefono);
+public Socio(String nombre, String apellido,Date fechaDeNacimiento, String sexo, int ci, String direccion,int telefono, double altura, Date fechaIngreso, int frecuencia) {
+	super(nombre, apellido, fechaDeNacimiento, sexo, ci, direccion, telefono);
 	this.fechaIngreso=fechaIngreso;
 	this.frecuencia=frecuencia;
-	
-	
+		
 }
 
  
