86b6118a924504ec26ff853baf79415456c6282b
Jose Luis Madrid <Jose Luis Madrid@10.211.55.3>
2020-04-02 21:17:43 -0300

    Cree la Clase Socio

--- src/semana3/Socio.java
+++ src/semana3/Socio.java
@@ -1,5 +1,27 @@
 package semana3;
 
-public class Socio {
+import java.sql.Date;
+
+public class Socio extends Persona {
+	private Date fechaIngreso;
+	private int frecuencia;
+	
+public Socio(String nombre, String apellido) {
+	super(nombre, apellido);
+	
+	
+	this.fechaIngreso=fechaIngreso;
+	this.frecuencia=frecuencia;
+	
+}
+public void crearSocio() {
+	
+	
+	
+}
+	
+	
+	
+	
 
 }
