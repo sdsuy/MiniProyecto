995811625587e34d5bb9485b19a174789750efdf
Jose Luis Madrid <Jose Luis Madrid@10.211.55.3>
2020-04-02 21:43:06 -0300

    Agregue los atributos propios de la clase socio en su constructor

--- src/semana3/Socio.java
+++ src/semana3/Socio.java
@@ -3,22 +3,18 @@ package semana3;
 import java.sql.Date;
 
 public class Socio extends Persona {
+	//Atributos
 	private Date fechaIngreso;
 	private int frecuencia;
-	
-public Socio(String nombre, String apellido) {
+	//Constructor
+public Socio(String nombre, String apellido, Date fechaIngreso, int frecuencia) {
 	super(nombre, apellido);
-	
-	
 	this.fechaIngreso=fechaIngreso;
 	this.frecuencia=frecuencia;
 	
 }
-public void crearSocio() {
-	
+public void crear(Socio socio ) {
 	//
-	
-	
 	
 }
 	
