7d009a889b476c9eac81fbb7587425b2fe4976c0
HP <HP@LAPTOP-5164SICD>
2020-04-04 10:57:18 -0300

    Agregué clase Cuota

---
+++ src/semana3/Cuota.java
@@ -0,0 +1,10 @@
+package semana3;
+import java.util.Date;
+
+public class Cuota{
+	//se declaran las variables valor, fecha pago  y fecha de vencimiento
+	
+	private double valor;
+	private Date fechaPago;
+	private Date fechaVencimiento;
+}
