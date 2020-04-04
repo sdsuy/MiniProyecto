e0803587c888c02fb5a896584253524ba1292001
diegoforichi <54032871+diegoforichi@users.noreply.github.com>
2020-04-04 10:52:02 -0300

    Update club.java

--- src/semana3/club.java
+++ src/semana3/club.java
@@ -1,14 +1,20 @@
 package semana3;
 
-public class club {
-	  
-    // Atributos
-		String nombre;
-		String direccion;
-		int telefono;
-		String web;
+public class Club {
+
+	public static void main(String[] args) {
 		
-    
+		private String nombre;
+		private String direccion;
+		private int telefono;
+		private String web;
+		
+		public Club(String nombre, String nombre, int telefono, String web) {
+			
+			this.nombre = nombre;
+			this.direccion = direccion;
+			this.telefono = telefono;
+			this.web = web;
 		
 	}
 
